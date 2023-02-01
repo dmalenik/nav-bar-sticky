9095b9827fd955b95abc8664ecfef709429f96b9
Dmitriy Malenik <dmalenik@gmail.com>
2023-02-01 22:23:51 +0100

    'feat: create Router'

--- nav-bar-sticky/src/index.tsx
+++ nav-bar-sticky/src/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Root from './Root';
+import ErrorPage from './ErrorPage';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
