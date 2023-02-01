1cec2a6a2f8ba071bb26f3fc0c9efeb7009d8cc1
Dmitriy Malenik <dmalenik@gmail.com>
2023-02-01 22:21:43 +0100

    'feat: create Navigation component'

---
+++ nav-bar-sticky/src/Navigation.tsx
@@ -0,0 +1,23 @@
+import './Navigation.css';
+
+const Navigation = () => {
+  return (
+    <>
+      <nav id='navigation'>
+        <ul>
+          <li>
+            <a href=''>About us</a>
+          </li>
+          <li>
+            <a href=''>Offer</a>
+          </li>
+          <li>
+            <a href=''>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
