a84b76302695413c750726127e9219ae91a225e1
Dmitriy Malenik <dmalenik@gmail.com>
2023-02-02 15:09:02 +0100

    'refactor: change Root component body'

--- nav-bar-sticky/src/Root.tsx
+++ nav-bar-sticky/src/Root.tsx
@@ -1,6 +1,12 @@
 import './Root.css';
 import Navigation from './Navigation';
 
-const Root = () => <Navigation />;
+const Root = () => {
+  return (
+    <div id='container'>
+      <Navigation />
+    </div>
+  );
+};
 
 export default Root;
