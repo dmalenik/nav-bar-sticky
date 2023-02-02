7b5912eddfc27b540ab91592512490aa33926d4c
Dmitriy Malenik <dmalenik@gmail.com>
2023-02-02 15:10:09 +0100

    'feat: add about-us path'

--- nav-bar-sticky/src/index.tsx
+++ nav-bar-sticky/src/index.tsx
@@ -4,13 +4,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import Root from './Root';
 import ErrorPage from './ErrorPage';
+import About from './About';
 import reportWebVitals from './reportWebVitals';
-
+// don't add child components as children
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: 'about-us',
+    element: <About />,
   },
 ]);
 const root = ReactDOM.createRoot(
