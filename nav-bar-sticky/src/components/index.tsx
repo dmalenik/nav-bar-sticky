338f9ea121b38a87a427066c6aaa7ff4b90fa195
Dmitriy Malenik <dmalenik@gmail.com>
2023-02-14 07:57:55 +0100

    feat: remove some re-export statements

--- nav-bar-sticky/src/components/index.tsx
+++ nav-bar-sticky/src/components/index.tsx
@@ -1,6 +1,2 @@
-export { About } from './About';
-export { Contact } from './Contact';
-export { ErrorPage } from './ErrorPage';
 export { Navigation } from './Navigation';
-export { Offer } from './Offer';
-export { RootComponent } from './RootComponent';
+export { App } from './App';
