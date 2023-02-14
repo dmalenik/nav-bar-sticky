2f5cbe422a6498bad1b3ba9686ae74c5403353ca
Dmitriy Malenik <dmalenik@gmail.com>
2023-02-14 07:56:49 +0100

    feat: remove routes

--- nav-bar-sticky/src/index.tsx
+++ nav-bar-sticky/src/index.tsx
@@ -1,57 +1,21 @@
 import React, { ReactNode } from 'react';
 import ReactDOM, { Root } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import './index.css';
-import {
-  RootComponent,
-  ErrorPage,
-  About,
-  Offer,
-  Contact,
-} from './components/index';
+import { App } from './components/index';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-
-type RouteObject = {
-  path: string;
-  element: JSX.Element;
-  errorElement?: JSX.Element;
-};
-type RouteObjectStorage = RouteObject[];
-
-const routes: RouteObjectStorage = [
-  {
-    path: '/',
-    element: <RootComponent />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: 'about-us',
-    element: <About />,
-  },
-  {
-    path: 'offer',
-    element: <Offer />,
-  },
-  {
-    path: 'contact',
-    element: <Contact />,
-  },
-];
 
 const root: Root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-const routerProvider: ReactNode = (
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={createBrowserRouter(routes)} />
-    </Provider>
-  </React.StrictMode>
+const renderChildren: ReactNode = (
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
-root.render(routerProvider);
+root.render(renderChildren);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
